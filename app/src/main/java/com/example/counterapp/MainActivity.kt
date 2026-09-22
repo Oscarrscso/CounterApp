8aab85f279291805f1d9b478ca655a83dc0282f9
Oscar Neiland <Oscarneiland@gmail.com>
2026-09-22 18:14:26 +0100

    counter button functionality added

--- app/src/main/java/com/example/counterapp/MainActivity.kt
+++ app/src/main/java/com/example/counterapp/MainActivity.kt
@@ -1,17 +1,23 @@
 package com.example.counterapp
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.counterapp.ui.theme.CounterAppTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +26,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             CounterAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    Counter(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +36,20 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun Counter(modifier: Modifier = Modifier) {
+    var count by remember { mutableStateOf(0) }
+    Button(
+        onClick = { count++ },
         modifier = modifier
-    )
+        ){
+        Text("Hello $count!")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun CounterPreview() {
     CounterAppTheme {
-        Greeting("Android")
+        Counter()
     }
 }
